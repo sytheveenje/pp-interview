7a4c63b331678a93108e117ff59681dc5fb8a93c
Sythe Veenje <hey@sythe.nl>
2024-12-24 11:04:45 +0100

    chore: install vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import IncrementCounter from './components/Calculator.vue';
+import Calculator from "./components/Calculator.vue";
+
+createApp({})
+    .component('Calculator', Calculator)
+    .mount('#app');
---
+++ resources/js/components/Calculator.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+<template>
+    <div class="bg-black text-white p-10">
+    <input type="text" class="text-black" />
+    <button @click="calculate">Calculate</button>
+    <p>Result</p>
+    </div>
+</template>
