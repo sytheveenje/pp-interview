d22215a2a86028322f3a04fd2a7ea19b2b9c5bef
Sythe Veenje <hey@sythe.nl>
2024-12-25 06:57:13 +0100

    Chore: find a package for complex calculations

--- app/Http/Controllers/Calculator/CalculatorController.php
+++ app/Http/Controllers/Calculator/CalculatorController.php
@@ -3,14 +3,29 @@
 namespace App\Http\Controllers\Calculator;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-
+use NXP\Exception\IncorrectBracketsException;
+use NXP\Exception\IncorrectExpressionException;
+use NXP\Exception\UnknownOperatorException;
+use NXP\Exception\UnknownVariableException;
+use NXP\MathExecutor;
 class CalculatorController extends Controller
 {
-    public function __invoke(Request $request, $input = null)
+    /**
+     * @throws IncorrectExpressionException
+     * @throws UnknownOperatorException
+     * @throws IncorrectBracketsException
+     * @throws UnknownVariableException
+     */
+    public function __invoke(Request $request, $input = null): JsonResponse
     {
+        // Get the input from the request
+        $input = 'sqrt(((((9*9)/12)+(13-4))*2)^2)';
+
         // Perform the calculation
-        $result = '31.5';
+        $executor = new MathExecutor();
+        $result = $executor->execute($input);
 
         // Return the result
         return response()->json(['input' => $input, 'result' => $result], 200);
